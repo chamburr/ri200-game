220079742f351c4cef6162ef242efbcca8e3c688
Han Cen <hi@chamburr.com>
2023-02-18 21:09:50 +0800

    feat: add page background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app" class="bg-light">
-    <BContainer class="d-flex flex-column justify-content-around min-vh-100 p-5">
+  <div id="app">
+    <BContainer class="d-flex flex-column justify-content-around min-vh-100 p-4 p-md-5">
       <Nuxt class="text-dark text-center" />
     </BContainer>
   </div>
@@ -11,6 +11,12 @@ export default {}
 </script>
 
 <style scoped lang="scss">
+#app {
+  background-image: url("~/assets/background.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
 @include media-breakpoint-up(md) {
   #app .container {
     max-width: 540px;
