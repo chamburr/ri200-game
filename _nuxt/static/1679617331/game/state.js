552e60fa8680055bbc174a2acb49d01f312a33c5
Han Cen <hi@chamburr.com>
2023-03-24 08:22:35 +0800

    a350cc4

---
+++ _nuxt/static/1679617331/game/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fri200-game\u002F_nuxt\u002Fstatic\u002F1679617331",layout:"default",error:a,state:{questions:[],response:[]},serverRendered:true,routePath:"\u002Fgame",config:{_app:{basePath:"\u002Fri200-game\u002F",assetsPath:"\u002Fri200-game\u002F_nuxt\u002F",cdnURL:a}}}}(null));
